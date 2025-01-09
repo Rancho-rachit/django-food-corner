63464d76a587428f82818b92040b2d9225e4588a
Rachit Agrawal <111473259+Rancho-rachit@users.noreply.github.com>
2025-01-09 13:35:08 +0000

    update naming

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dummy1 project.
+WSGI config for main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dummy1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
 
